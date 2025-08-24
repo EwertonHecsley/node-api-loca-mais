9d1a0e0e1566a3d6571a9a54de5bda9eff317eb7
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2025-08-24 11:07:54 -0300

    test: add unit test in useCase FindById

--- src/application/user/useCase/Find.ts
+++ src/application/user/useCase/Find.ts
@@ -1,25 +1,25 @@
-import type { User } from "@src/core/domain/user/entity/User";
-import type { UserGateway } from "@src/core/domain/user/gateway/UserGateway";
-import { InternalServerError } from "@src/shared/errors/custom/InternalServerError";
-import { NotFoundError } from "@src/shared/errors/custom/NotFoundError";
-import { left, right, type Either } from "@src/shared/utils/Either";
+import type { User } from '@src/core/domain/user/entity/User';
+import type { UserGateway } from '@src/core/domain/user/gateway/UserGateway';
+import { InternalServerError } from '@src/shared/errors/custom/InternalServerError';
+import { NotFoundError } from '@src/shared/errors/custom/NotFoundError';
+import { left, right, type Either } from '@src/shared/utils/Either';
 
 type RequestUser = {
-    id:string;
-}
+  id: string;
+};
 
-type ResponseUser = Either<NotFoundError | InternalServerError,User>;
+type ResponseUser = Either<NotFoundError | InternalServerError, User>;
 
 export class FindByIdUserUseCase {
-  constructor(private readonly userGateway:UserGateway){}
+  constructor(private readonly userGateway: UserGateway) {}
 
-  async execute({id}:RequestUser):Promise<ResponseUser>{
+  async execute({ id }: RequestUser): Promise<ResponseUser> {
     try {
       const user = await this.userGateway.findById(id);
-      if(!user) return left(new NotFoundError('User not found'));
+      if (!user) return left(new NotFoundError('User not found'));
       return right(user);
     } catch (error) {
-      if(!(error instanceof NotFoundError)){
+      if (!(error instanceof NotFoundError)) {
         return left(new InternalServerError());
       }
       return left(error);
